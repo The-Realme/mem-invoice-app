b3ac49ed84843f7b3cc5c04a273e9c2c40de7a3b
Shahid <shahidashraf7348@gmail.com>
2026-07-08 20:19:29 +0530

    initial all files commited

---
+++ main.js
@@ -0,0 +1,125 @@
+const { app, BrowserWindow } = require("electron");
+const { spawn } = require("child_process");
+const path = require("path");
+const http = require("http");
+
+let serverProcess;
+let splash;
+let mainWindow;
+
+function startServer() {
+
+    serverProcess = spawn("node", ["server.js"], {
+
+        shell: true,
+        stdio: "inherit"
+
+    });
+
+}
+
+function waitForServer(callback) {
+
+    const timer = setInterval(() => {
+
+        http.get("http://localhost:3000", () => {
+
+            clearInterval(timer);
+
+            callback();
+
+        }).on("error", () => {
+
+            // Server still starting...
+        });
+
+    }, 500);
+
+}
+
+function createSplash() {
+
+    splash = new BrowserWindow({
+
+        width: 500,
+        height: 300,
+
+        frame: false,
+
+        resizable: false,
+
+        alwaysOnTop: true,
+
+        autoHideMenuBar: true,
+
+        icon: path.join(
+            __dirname,
+            "public",
+            "assets",
+            "favicon.ico"
+        )
+
+    });
+
+    splash.loadFile(path.join(__dirname, "electron", "splash.html"));
+
+}
+
+function createMainWindow() {
+
+    mainWindow = new BrowserWindow({
+
+        width: 1400,
+        height: 900,
+
+        autoHideMenuBar: true,
+
+        title: "MEM Invoice System",
+        icon: path.join(
+            __dirname,
+            "public",
+            "assets",
+            "favicon.ico"
+        ),
+
+        show: false
+
+    });
+
+    mainWindow.loadURL("http://localhost:3000");
+
+    mainWindow.once("ready-to-show", () => {
+
+        splash.close();
+
+        mainWindow.show();
+
+    });
+
+}
+
+app.whenReady().then(() => {
+
+    startServer();
+
+    createSplash();
+
+    waitForServer(() => {
+
+        createMainWindow();
+
+    });
+
+});
+
+app.on("window-all-closed", () => {
+
+    if (serverProcess) {
+
+        serverProcess.kill();
+
+    }
+
+    app.quit();
+
+});
