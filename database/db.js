fda4a2a0f01ca2c233555def93fe3f30ab0a54db
Shahid <shahidashraf7348@gmail.com>
2026-07-16 23:29:02 +0530

    final probably

--- database/db.js
+++ database/db.js
@@ -1,17 +1,26 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+
+//     user: "postgres",
+
+//     host: "localhost",
+
+//     database: "mem_office",
+
+//     password: "Shahi2006@Ashu",
+
+//     port: 5432
+
+// });
+
+// module.exports = pool;
+
+require("dotenv").config();
+
 const pool = new Pool({
-
-    user: "postgres",
-
-    host: "localhost",
-
-    database: "mem_office",
-
-    password: "Shahi2006@Ashu",
-
-    port: 5432
-
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-
-module.exports = pool;
