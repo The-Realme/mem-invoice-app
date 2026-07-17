609949b16a7abb9bb6fd750d489d12ca962c3c81
Shahid <shahidashraf7348@gmail.com>
2026-07-17 20:29:25 +0530

    This is updated version of db.js

--- database/db.js
+++ database/db.js
@@ -1,23 +1,19 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const pool = new Pool(
-    isProduction
-        ? {
-              connectionString: process.env.DATABASE_URL,
-              ssl: {
-                  rejectUnauthorized: false
-              }
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+              rejectUnauthorized: false
           }
-        : {
-              host: process.env.DB_HOST,
-              port: process.env.DB_PORT || 5432,
-              user: process.env.DB_USER,
-              password: process.env.DB_PASSWORD,
-              database: process.env.DB_NAME
-          }
-);
+      })
+    : new Pool({
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT || 5432,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME
+      });
 
 module.exports = pool;
