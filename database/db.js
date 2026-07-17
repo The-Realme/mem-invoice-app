76a2562388f46f59bd7b0752f85914866b298e7f
Shahid <shahidashraf7348@gmail.com>
2026-07-17 18:38:51 +0530

    Making our website Deploy ready

--- database/db.js
+++ database/db.js
@@ -1,11 +1,16 @@
 const { Pool } = require("pg");
-
 require("dotenv").config();
 
 const pool = new Pool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl:
+        process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false
 });
+
 module.exports = pool;
